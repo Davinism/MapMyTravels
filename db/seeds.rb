6faae3091e3d7cdf91330146ffd7e74ad25f6b36
Davin Kim <davinkim2015@u.northwestern.edu>
2016-08-31 17:08:04 -0700

    Seeded the database with some users. Tried to get the slideshow working in the authentication page, but failed.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,31 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create!(email: "kevinbacon@gmail.com",
+              password: "kevinbaconthebacon",
+              first_name: "Kevin",
+              last_name: "Bacon",
+              date_of_birth: "07/08/1958",
+              gender: "M",
+              )
+
+User.create!(email: "taylorswift@gmail.com",
+              password: "onceuponadecember",
+              first_name: "Taylor",
+              last_name: "Swift",
+              date_of_birth: "12/13/1989",
+              gender: "F"
+              )
+
+User.create!(email: "conan@teamcoco.com",
+              password: "teamcoco",
+              first_name: "Conan",
+              last_name: "O'brien",
+              date_of_birth: "04/18/1963",
+              gender: "M"
+              )
+
+User.create!(email: "jschlansky@teamcoco.com",
+              password: "starwars",
+              first_name: "Jordan",
+              last_name: "Schlansky",
+              date_of_birth: "04/13/1973",
+              gender: "M"
+              )
