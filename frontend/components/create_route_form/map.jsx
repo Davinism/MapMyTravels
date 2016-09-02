e4ddd76eb5ea862132b0c98db51fbeaf733a91b6
Davin Kim <davinkim2015@u.northwestern.edu>
2016-09-02 15:31:45 -0700

    Got the distance calculator working.

--- frontend/components/create_route_form/map.jsx
+++ frontend/components/create_route_form/map.jsx
@@ -81,8 +81,8 @@ class AppMap extends React.Component {
 
       const newCoord = [latString, lngString].join(", ");
 
-      // this.prev = this.current;
-      // this.current = newCoord;
+      this.prev = this.current;
+      this.current = newCoord;
 
       this.setState({coords: this.state.coords.concat(newCoord)});
 
@@ -120,42 +120,38 @@ class AppMap extends React.Component {
   }
 
   calculateDistance() {
-    if (this.state.coords.length > 1) {
-      const origins = this.state.coords.slice(0, this.state.coords.length - 1);
-      const destinations = this.state.coords.slice(1);
-
-      const originsLatLng = origins.map( coord => {
-        const origin = coord.split(", ");
-        return new google.maps.LatLng(origin[0], origin[1]);
-      });
-
-      const destinationsLatLng = destinations.map( coord => {
-        const dest = coord.split(", ");
-        return new google.maps.LatLng(dest[0], dest[1]);
-      });
+    if (this.prev) {
+      const origin = this.prev.split(", ");
+      const destination = this.current.split(", ");
+      const originLatLng = new google.maps.LatLng(
+        parseFloat(origin[0]),
+        parseFloat(origin[1])
+      );
+      const destinationLatLng = new google.maps.LatLng(
+        parseFloat(destination[0]),
+        parseFloat(destination[1])
+      );
 
       let service = new google.maps.DistanceMatrixService();
-
-      let totalDistance = 0;
 
       const thisMap = this;
 
       const callback = (response, status) => {
+        let distance;
         if (status === 'OK') {
-          var distance = response.rows[0].elements[0].distance.value;
-          totalDistance = totalDistance + (distance / 1609.34);
+          distance = response.rows[0].elements[0].distance.value / 1609.34;
+        } else {
+          distance = 0;
         }
-        thisMap.setState({totalDistance: totalDistance});
+        thisMap.setState({totalDistance: thisMap.state.totalDistance + distance});
       };
 
-      for (var i = 0; i < originsLatLng.length; i++) {
-        service.getDistanceMatrix({
-          origins: [originsLatLng[i]],
-          destinations: [destinationsLatLng[i]],
-          travelMode: 'DRIVING',
-          unitSystem: google.maps.UnitSystem.IMPERIAL
-        }, callback);
-      }
+      service.getDistanceMatrix({
+        origins: [originLatLng],
+        destinations: [destinationLatLng],
+        travelMode: 'DRIVING',
+        unitSystem: google.maps.UnitSystem.IMPERIAL
+      }, callback);
     }
   }
 
@@ -165,13 +161,13 @@ class AppMap extends React.Component {
       return <li key={ index }>{coord}</li>;
     });
 
-    console.log(this.state.totalDistance);
-
     return (
       <section className="map-data-container">
         <div id="map-container" ref="map"></div>
         <ul>
           { coordsList }
+          <br />
+          <li>{ this.state.totalDistance }</li>
         </ul>
       </section>
     );
