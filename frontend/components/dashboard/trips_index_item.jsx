e76c20b775c3d3350a08aa6ac0f1a3991acd6987
Davin Kim <davinkim2015@u.northwestern.edu>
2016-09-06 09:14:41 -0700

    Finished up styling the trips index.

--- frontend/components/dashboard/trips_index_item.jsx
+++ frontend/components/dashboard/trips_index_item.jsx
@@ -21,19 +21,19 @@ class TripsIndexItem extends React.Component {
           <div className="trip-title">{this.props.trip.title}</div>
           <div className="trip-distance">
             Distance: <br />
-            <span>{this.props.trip.route_distance}</span> mi
+          <span>{this.props.trip.route_distance.toFixed(2)}</span> mi
           </div>
         </section>
 
         <section className="trip-dates">
-          <div>Dates</div>
-          <div><span>Start:</span><span>{this.props.trip.start_date}</span></div>
-          <div><span>End:</span><span>{this.props.trip.end_date}</span></div>
+          <div className="title">Dates</div>
+          <div className="date"><span>Start:</span><span>{this.props.trip.start_date}</span></div>
+          <div className="date"><span>End:</span><span>{this.props.trip.end_date}</span></div>
         </section>
 
         <section className="expenditure">
-          <div>Expenditure</div>
-          <div>{this.props.trip.expenditure}</div>
+          <div className="title">Expenditure</div>
+          <div className="cost"><span>$</span> {this.props.trip.expenditure.toFixed(2)}</div>
         </section>
       </div>
     );
