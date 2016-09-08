f8c3c6337a443d692b06a43dea82e70470e04840
Davin Kim <davinkim2015@u.northwestern.edu>
2016-09-08 09:04:20 -0700

    Implemented the friend search bar.

--- frontend/components/find_friends/find_friends.jsx
+++ frontend/components/find_friends/find_friends.jsx
@@ -7,28 +7,65 @@ import UserItem from './user_item';
 class FindFriends extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      inputVal: ""
+    };
+
+    this.handleInput = this.handleInput.bind(this);
+  }
+
+  handleInput(event) {
+    this.setState({inputVal: event.currentTarget.value});
+  }
+
+  matches() {
+    const matches = [];
+    for (let userId in this.props.users) {
+      let sub = this.props.users[userId].name.slice(0, this.state.inputVal.length);
+      if (sub.toLowerCase() === this.state.inputVal.toLowerCase()) {
+        matches.push(this.props.users[userId]);
+      }
+    }
+
+    return matches;
   }
 
   render() {
-    const usersList = [];
+    // const usersList = [];
     const masonryOptions = { transitionDuration: 0 };
     let friendStatus;
 
-    for (let userId in this.props.users) {
-      if (this.props.friends.hasOwnProperty(userId)) {
+    // for (let userId in this.props.users) {
+    //   if (this.props.friends.hasOwnProperty(userId)) {
+    //     friendStatus = "Following";
+    //   } else {
+    //     friendStatus = "Not Following";
+    //   }
+    //
+    //   usersList.push(<UserItem key={userId}
+    //     user={this.props.users[userId]}
+    //     currentUser={this.props.currentUser}
+    //     destroyFriendTagging={this.props.destroyFriendTagging}
+    //     createFriendTagging={this.props.createFriendTagging}
+    //     friendStatus={friendStatus} />
+    //   );
+    // }
+
+    let results = this.matches().map((user, index) => {
+      if (this.props.friends.hasOwnProperty(user.id)) {
         friendStatus = "Following";
       } else {
         friendStatus = "Not Following";
       }
 
-      usersList.push(<UserItem key={userId}
-        user={this.props.users[userId]}
+      return <UserItem key={user.id}
+        user={user}
         currentUser={this.props.currentUser}
         destroyFriendTagging={this.props.destroyFriendTagging}
         createFriendTagging={this.props.createFriendTagging}
-        friendStatus={friendStatus} />
-      );
-    }
+        friendStatus={friendStatus} />;
+    });
 
     return (
       <div>
@@ -36,12 +73,12 @@ class FindFriends extends React.Component {
         <div className="find-friends-page-title">
           <div>Find Friends</div>
           <div className="search" method="get">
-            <input type="text" size="40" placeholder="Type Name"/>  
+            <input type="text" size="40" placeholder="Type Name" onChange={this.handleInput}/>
           </div>
         </div>
         <div>
           <ul className="users-gallery">
-            {usersList}
+            {results}
           </ul>
         </div>
       </div>
