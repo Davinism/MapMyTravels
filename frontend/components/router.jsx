c6afda210d274cc8c6ba9f069f637d6cf059c932
Davin Kim <davinkim2015@u.northwestern.edu>
2016-10-06 16:54:23 -0700

    Reverting again.

--- frontend/components/router.jsx
+++ frontend/components/router.jsx
@@ -90,7 +90,6 @@ class AppRouter extends React.Component{
     return(
       <Router history={ hashHistory }>
         <Route path="/" component={ App }>
-          // <IndexRoute component={ SessionFormContainer } />
           <Route path="/dashboard" component={ DashboardContainer } onEnter={this._ensureLoggedIn} />
           <Route path="/login" component={ SessionFormContainer } onEnter={this._redirectIfLoggedIn}/>
           <Route path="/signup" component={ SessionFormContainer } onEnter={this._redirectIfLoggedIn}/>
